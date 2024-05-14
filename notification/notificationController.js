36e3791899aa491d4b5286feee753c892c5047d2
KarayaOne <30974631+KarayaOne@users.noreply.github.com>
2024-05-14 18:43:19 +0500

    fixed notif topic

--- notification/notificationController.js
+++ notification/notificationController.js
@@ -110,7 +110,7 @@ const sendNotification = async (req, res) => {
 // Send notifications to FCM topics
 const sendToFirebase = async (message) => {
   // Setting topic and image variable
-  const topic = "blueFootball";
+  const topic = "saad";
 
   // Initliaze message function
   const { messaging } = firebase;
