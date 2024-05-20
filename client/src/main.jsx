fc0bced45e94a406279c2f89eabdb7462c732c31
KarayaOne <30974631+KarayaOne@users.noreply.github.com>
2024-05-20 19:28:17 +0500

    Remove React Favicon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Favicon from "react-favicon";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Favicon url="https://raw.githubusercontent.com/ziaaurrehman/score2admin/main/client/src/assets/favicon.ico" />
     <App />
   </BrowserRouter>
 );
